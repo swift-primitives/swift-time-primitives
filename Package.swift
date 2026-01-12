44c752722c92780e54aa1fa11776c626a17f14ef
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-12 20:14:48 +0100

    Convert dependencies to remote URLs

--- Package.swift
+++ Package.swift
@@ -18,9 +18,9 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(path: "../swift-standard-library-extensions"),
-        .package(path: "../swift-dimension-primitives"),
-        .package(path: "../swift-test-primitives"),
+        .package(url: "https://github.com/swift-primitives/swift-standard-library-extensions.git", from: "0.0.1"),
+        .package(url: "https://github.com/swift-primitives/swift-dimension-primitives.git", from: "0.0.1"),
+        .package(url: "https://github.com/swift-primitives/swift-test-primitives.git", from: "0.0.1"),
     ],
     targets: [
         .target(
